81c5e950c2c4f765ecab566899a8383027fa7f72
Sai Sawant <saisawant2003@gmail.com>
2023-04-10 13:32:06 +0530

    route handling added

--- login_page/src/App.js
+++ login_page/src/App.js
@@ -1,8 +1,18 @@
 import LoginPage from "./components/LoginPage/LoginPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <LoginPage/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- login_page/src/components/LoginPage/Form.jsx
+++ login_page/src/components/LoginPage/Form.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import FormControl from "./FormControl";
 import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 
@@ -6,9 +7,9 @@ export const Form = () => {
 
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
-    
+    const navigate=useNavigate()
   return (
-    <form action="" onSubmit={(e)=>e.preventDefault()}>
+    <form action="" onSubmit={()=> navigate('/home')}>
       <FormControl type={"email"} placeholder={"Email ID"} icon={faUser}  setInput={setEmail} />
       <FormControl type={"password"} placeholder={"Password"} icon={faLock} setInput={setPassword}/>
       <button type="submit">Login</button>
